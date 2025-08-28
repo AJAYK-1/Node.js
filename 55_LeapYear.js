631a7d07511fdd2197a88a0f2f80aa28894391d0
AJAYK-1 <ajaykumar1shy@gmail.com>
2025-08-28 23:27:37 +0530

    "Leap Year"

---
+++ 55_LeapYear.js
@@ -0,0 +1,11 @@
+// Given a year, report if it is a leap year...
+
+
+const isLeapYear = year => year % 4 === 0 ? true : false
+
+console.log(isLeapYear(2020));
+console.log(isLeapYear(2021));
+console.log(isLeapYear(2022));
+console.log(isLeapYear(2023));
+console.log(isLeapYear(2024));
+console.log(isLeapYear(2025));
