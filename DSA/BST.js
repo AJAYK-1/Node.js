3fde38f84eb427b022b4d16a37a8a6c41187eb20
AJAYK-1 <ajaykumar1shy@gmail.com>
2025-08-30 14:31:48 +0530

    "BFS and DFS"

--- DSA/BST.js
+++ DSA/BST.js
@@ -39,7 +39,7 @@ class BST {
         }
     }
 
-    search(value) {
+    includes(value) {
         if (!this.root) return `The tree is empty...`
         if (this.root.data === value) return `${value} is in the root node.`
 
@@ -52,22 +52,61 @@ class BST {
         return `No element found...`
     }
 
-    bfs(){
+    bfs() {
         let queue = []
         let elements = []
         let temp = this.root
 
         queue.push(temp)
-        
+        while (queue.length) {
+            temp = queue.shift()
+            elements.push(temp.data)
+
+            if (temp.leftNode) queue.push(temp.leftNode)
+            if (temp.rightNode) queue.push(temp.rightNode)
+        }
+        return elements
+    }
+
+    dfsPreOrder(node = this.root, data = []) {
+        if (node === null) return data
+        data.push(node.data)
+
+        if (node.leftNode) this.dfsPreOrder(node.leftNode, data)
+        if (node.rightNode) this.dfsPreOrder(node.rightNode, data)
+        return data
+    }
+
+    dfsPostOrder(node = this.root, data = []) {
+        if (node === null) return data
+        if (node.leftNode) this.dfsPostOrder(node.leftNode, data)
+        if (node.rightNode) this.dfsPostOrder(node.rightNode, data)
+
+        data.push(node.data)
+        return data
+    }
+
+    dfsInOrder(node = this.root, data = []) {
+        if (node === null) return data
+
+        if (node.leftNode) this.dfsInOrder(node.leftNode, data)
+        data.push(node.data)
+        if (node.rightNode) this.dfsInOrder(node.rightNode, data)
+        return data
     }
 }
 
 const tree = new BST()
-tree.insert(10)
-tree.insert(5)
-tree.insert(12)
+tree.insert(50)
+tree.insert(30)
+tree.insert(75)
+tree.insert(60)
+tree.insert(80)
 tree.insert(20)
 console.log(tree);
-console.log(tree.search(20));
-console.log(tree.search(50));
-
+console.log(tree.includes(20));
+console.log(tree.includes(50));
+console.log('Breadth First Search: ', tree.bfs());
+console.log('DFS Pre-Order: ', tree.dfsPreOrder());
+console.log('DFS Post-Order: ', tree.dfsPostOrder());
+console.log('DFS In-Order: ', tree.dfsInOrder());
